19fba44ba2918fad584ce1fb929fccbf9b91a4b0
Dustin Homan <dustinhoman@Dustins-MacBook-Pro.local>
2016-11-12 09:49:19 -0700

    Building CNN model

--- lib/model.py
+++ lib/model.py
@@ -1,10 +1,22 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.ops import rnn_cell, seq2seq
-from tensorflow.contrib import grid_rnn
 
-
-class Model(object):
+class CNN(object):
     def __init__(self, args, infer=False):
+        def weight_variable(shape):
+            initial = tf.truncated_normal(shape, stddev=0.1)
+            return tf.Variable(initial)
+
+        def bias_variable(shape):
+            initial = tf.constant(0.1, shape=shape)
+            return tf.Variable(initial)
+
+        def conv2d(x, W):
+          return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+        def max_pool_2x2(x):
+          return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                                strides=[1, 2, 2, 1], padding='SAME')
+
         self.W_conv1 = weight_variable([5,5,1,32])
         self.b_conv1 = bais_variable([32])
